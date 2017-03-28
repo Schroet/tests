dd85be4319207f82236ca0eab3c93524d764eede
Anatoliy Velikokhatskiy <avel@diceus.com>
2017-03-28 13:09:54 +0300

    Additional suites were added

---
+++ BNFSA/src/suitRunner/Suite2.java
@@ -0,0 +1,22 @@
+package suitRunner;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import adminportalTests.Test2;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	
+	
+	Test2.class
+	
+	
+
+}) 
+
+public class Suite2 {
+	 
+		
+		
+	}
